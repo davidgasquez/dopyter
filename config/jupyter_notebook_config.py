eb0bf377f37e418cbf35104a44407a14dad05a20
David Gasquez <davidgasquez@gmail.com>
2016-10-21 16:03:06 +0200

    Add configuration

---
+++ config/jupyter_notebook_config.py
@@ -0,0 +1,9 @@
+# Notebook standard configuration
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 8888
+c.NotebookApp.open_browser = False
+
+# Enable Matplotlib Inline by default
+c.InteractiveShellApp.exec_lines = []
+c.InteractiveShellApp.extensions = ['matplotlib']
+c.InteractiveShellApp.matplotlib = 'inline'
